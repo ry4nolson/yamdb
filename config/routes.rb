422df5cfd93e531932a50b94de7ae8f6fe45ccbb
Ryan Olson <ry4nolson@gmail.com>
2017-11-17 00:03:45 +0000

    add root route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#home'
   
   get 'welcome/home', to: 'welcome#home'
   get 'welcome/about', to: 'welcome#about'
