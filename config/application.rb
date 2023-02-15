a7022ee62dfc6bdfe49330eb0007da413c13eaf5
Ryan Olson <ryan.olson@fsgi.com>
2023-02-15 17:30:30 -0600

    try something

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,22 @@
 require_relative "boot"
 
-require "rails/all"
+require "rails"
+
+%w(
+  action_controller/railtie
+  action_view/railtie
+  action_mailer/railtie
+  active_job/railtie
+  action_cable/engine
+  action_mailbox/engine
+  action_text/engine
+  rails/test_unit/railtie
+).each do |railtie|
+  begin
+    require railtie
+  rescue LoadError
+  end
+end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
