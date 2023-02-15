e8a0eaf888cd3306068e177c8d9e73e989d87261
Ryan Olson <ryan.olson@fsgi.com>
2023-02-15 17:57:44 -0600

    remove more stuff

--- config/application.rb
+++ config/application.rb
@@ -5,12 +5,6 @@ require "rails"
 %w(
   action_controller/railtie
   action_view/railtie
-  action_mailer/railtie
-  active_job/railtie
-  action_cable/engine
-  action_mailbox/engine
-  action_text/engine
-  rails/test_unit/railtie
 ).each do |railtie|
   begin
     require railtie
